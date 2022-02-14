92e89e4a6a74e20a5433f8dca48c7db6a91fe305
George Ahmarani <george.ahmarani@cgi.com>
2022-02-14 13:41:43 -0500

    Correcting the name of the file and testing using the inputs name

---
+++ cypress/integration/werk-it-login-page/signup.spec.js
@@ -0,0 +1,35 @@
+describe('Testing the signup', () => {
+    beforeEach(() => {
+        cy.visit('https://staging.tiered-planet.net/werk-it')
+      })
+    it('Checking for the creating profile button', () => {
+        cy.get('.mx-lg-3')
+        .click()
+    })
+    it('Filling all input fields in the signup form', () => {
+        cy.get('.mx-lg-3')
+        .click()
+        .get('input[name = "firstName"]')
+        // or use the target option
+        //.get(':nth-child(1) > label > input')
+        .type("Lisa")
+        .get('input[name = "lastName"]')
+        // or use the target option
+        //.get(':nth-child(2) > label > input')
+        .type('jefferson')
+        .get('input[name = "email"]')
+        // or use the target option
+        //.get(':nth-child(3) > label > input')
+        .type("lisa.jefferson@gmail.com")
+        .get('input[name = "username"]')
+        // or use target option
+        //.get(':nth-child(4) > label > input')
+        .type("lisaJ")
+        .get('input[name = "password"]')
+        // or use target option
+        //.get(':nth-child(5) > label > input')
+        .type("lisa123")
+        .get('[type="submit"]')
+        .click()
+    })
+});
